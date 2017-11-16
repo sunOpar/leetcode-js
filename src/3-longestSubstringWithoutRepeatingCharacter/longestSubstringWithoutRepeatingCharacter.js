f6d830dcc332c3aaded3820fc3d22c348bee5128
sunopar <xygjsj@139.com>
2017-11-16 11:06:01 +0800

    optmize question 3

--- src/3-longestSubstringWithoutRepeatingCharacter/longestSubstringWithoutRepeatingCharacter.js
+++ src/3-longestSubstringWithoutRepeatingCharacter/longestSubstringWithoutRepeatingCharacter.js
@@ -1,22 +1,21 @@
+/**
+ * 通过Map来解决，i是左坐标，j是右坐标。增加时发现重复，就移动左坐标
+ * @param {String} s 
+ * @return {Number} maxLength
+ */
 const lengthOfLongestSubstring = s => {
   let maxLength = 0;
-  const stringSet = new Set();
+  const map = new Map();
   const { length } = s;
-  for (let i = 0; i < length - maxLength; i++) {
-    let accumulate = 0;
-    stringSet.clear();
-    for (let j = i; j < length; j++) {
-      stringSet.add(s[j]);
-      if (stringSet.size > accumulate) {
-        accumulate++;
-        if (accumulate > maxLength) {
-          maxLength = accumulate;
-        }
-      } else {
-        break;
-      }
+  for (let j = 0, i = 0; j < length; j++) {
+    if (map.has(s[j])) {
+      // 如果重复，i坐标右移
+      i = Math.max(map.get(s[j]) + 1, i);
     }
+    maxLength = Math.max(maxLength, j - i + 1);
+    map.set(s[j], j);
   }
+
   return maxLength;
 };
 module.exports = lengthOfLongestSubstring;
--- src/3-longestSubstringWithoutRepeatingCharacter/longestSubstringWithoutRepeatingCharacter.test.js
+++ src/3-longestSubstringWithoutRepeatingCharacter/longestSubstringWithoutRepeatingCharacter.test.js
@@ -1,8 +1,15 @@
+/**
+ * Given "abcabcbb", the answer is "abc", which the length is 3.
+ * Given "bbbbb", the answer is "b", with the length of 1.
+ * Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring,
+ *  "pwke" is a subsequence and not a substring.
+ */
 const lengthOfLongestSubstring = require('./longestSubstringWithoutRepeatingCharacter');
 describe('longestSubstringWithoutRepeatingCharacter', () => {
   test('longestSubstringWithoutRepeatingCharacter', () => {
     expect(lengthOfLongestSubstring('abcabcbb')).toBe(3);
     expect(lengthOfLongestSubstring('bbbbb')).toBe(1);
     expect(lengthOfLongestSubstring('pwwkew')).toBe(3);
+    expect(lengthOfLongestSubstring('abcade')).toBe(5);
   });
 });
