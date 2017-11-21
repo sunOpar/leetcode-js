f58259e85faeaf6ead30cc95458ad8544b9e66f8
sunopar <xygjsj@139.com>
2017-11-21 15:24:15 +0800

    optmize formmation

--- src/utils/sorts/quickSort/index.js
+++ src/utils/sorts/quickSort/index.js
@@ -2,11 +2,9 @@ const quickSort = nums => {
   _quickSort(nums, 0, nums.length - 1);
   return nums;
 };
-
 const _quickSort = (nums, left, right) => {
-  let pivot;
   if (left < right) {
-    pivot = partition(nums, left, right);
+    const pivot = partition(nums, left, right);
     if (left < pivot) {
       _quickSort(nums, left, pivot - 1);
     }
@@ -16,13 +14,13 @@ const _quickSort = (nums, left, right) => {
   }
 };
 const partition = (nums, left, right) => {
-  const pivot = Math.floor((right + left) / 2);
+  const pivot = Math.floor((left + right) / 2);
   const pivotNum = nums[pivot];
   while (left < right) {
     while (nums[left] < pivotNum && left < right) {
       left++;
     }
-    while (nums[right] > pivotNum && left < right) {
+    while (nums[right] > pivotNum && right > left) {
       right--;
     }
     if (left < right) {
