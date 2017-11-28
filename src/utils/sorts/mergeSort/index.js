e6c95d18334eced2c1827ae59afc28384e78ddeb
sunopar <xygjsj@139.com>
2017-11-28 15:02:58 +0800

    change merge sort to in-place

--- src/utils/sorts/mergeSort/index.js
+++ src/utils/sorts/mergeSort/index.js
@@ -1,34 +1,65 @@
+/**
+ * 参考： http://blog.csdn.net/acdreamers/article/details/24244643
+ */
+
 const mergeSort = arr => {
-  const result = _mergeSort(arr);
-  return result;
+  const leftIndex = 0;
+  const rightIndex = arr.length - 1;
+  _mergeSort(leftIndex, rightIndex, arr);
+  return arr;
 };
-const _mergeSort = arr => {
-  if (arr.length === 1) {
-    return arr;
+const _mergeSort = (leftIndex, rightIndex, arr) => {
+  if (leftIndex < rightIndex) {
+    const middleIndex = Math.floor((rightIndex + leftIndex) / 2);
+    _mergeSort(leftIndex, middleIndex, arr);
+    _mergeSort(middleIndex + 1, rightIndex, arr);
+    merge(leftIndex, rightIndex, middleIndex, arr);
   }
-  const middleIndex = Math.floor(arr.length / 2);
-  const left = arr.slice(0, middleIndex);
-  const right = arr.slice(middleIndex, arr.length);
-  return twoArraySort(_mergeSort(left), _mergeSort(right));
 };
-const twoArraySort = (left, right) => {
+const merge = (leftIndex, rightIndex, middleIndex, arr) => {
   // 定义两个指针
-  let i = 0,
-    j = 0;
-  const { length: leftLength } = left;
-  const { length: rightLength } = right;
-  const resultArray = [];
-  while (i < leftLength || j < rightLength) {
-    const leftValue = left[i];
-    const rightValue = right[j];
-    if (leftValue < rightValue || rightValue === undefined) {
-      resultArray.push(left[i++]);
-    } else if (leftValue > rightValue || leftValue === undefined) {
-      resultArray.push(right[j++]);
-    } else if (leftValue === rightValue) {
-      resultArray.push(left[i++], right[j++]);
+  let i = leftIndex;
+  let j = middleIndex + 1;
+  /**
+   * 首先移动i，直到arr[i]>arr[j]，i经过的这些数组，是两个数组中最小的，然后接着
+   * 移动j，直到arr[j]>arr[i]，j经过的数组，都是第二小的，然后把第二小的数组移到第一组的后面；如此反复
+   */
+  while (i < j && j <= rightIndex) {
+    while (arr[i] <= arr[j] && i < j) {
+      i++;
     }
+    let index = j;
+    while (arr[j] <= arr[i] && j <= rightIndex) {
+      j++;
+    }
+    convent(arr, i, index, j);
+    i += j - index;
   }
-  return resultArray;
+};
+/**
+ * [i,index)和[index,j)交换
+ * 交换利用手摇算法，即三次倒序完成反转，先反转[i,index),再反转[index,j)。最后反转[i,j]
+ * @param {Array} arr
+ * @param {Number} i
+ * @param {Number} index
+ * @param {Number} j
+ */
+const convent = (arr, i, index, j) => {
+  reverse(arr, i, index - 1);
+  reverse(arr, index, j - 1);
+  reverse(arr, i, j - 1);
+};
+/**
+ * 反转，前后两个指针向中间依次扫描交换。
+ * @param {Array} arr
+ * @param {Number} i
+ * @param {Number} j
+ */
+const reverse = (arr, i, j) => {
+  while (i < j) {
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+    j--;
+    i++;
+  }
 };
 module.exports = mergeSort;
