22537db855178c042369743c73a23d63446d54d2
sunopar <xygjsj@139.com>
2018-03-07 21:00:46 +0800

    :hankey: add Q14 code

--- src/14-longestCommonPrefix/longestCommonPrefix.js
+++ src/14-longestCommonPrefix/longestCommonPrefix.js
@@ -2,7 +2,25 @@
  * @param {string[]} strs
  * @return {string}
  */
-function longestCommonPrefix(strs){
-
+function longestCommonPrefix(strs) {
+  if (strs.length === 0) {
+    return ''
+  }
+  if (strs.length === 1) {
+    return strs[0]
+  }
+  let commonPrefix = '';
+  let hasCommonPrefix = true;
+  let prefixIndex = 0;
+  do {
+    const firstElement = strs[0][prefixIndex] || '';
+    hasCommonPrefix = strs.every(item => item[prefixIndex] === firstElement);
+    if (hasCommonPrefix) {
+      // 如果strs为['','']，firstElement有可能为undefined
+      commonPrefix += firstElement || '';
+    }
+    prefixIndex++;
+  } while (hasCommonPrefix);
+  return commonPrefix;
 }
 module.exports = longestCommonPrefix;
--- src/14-longestCommonPrefix/longestCommonPrefix.test.js
+++ src/14-longestCommonPrefix/longestCommonPrefix.test.js
@@ -4,6 +4,11 @@ const func = require('./longestCommonPrefix');
 
 describe('longest common prefix', () => {
   test('common test', () => {
-    expect(func(['abc', 'abdf', 'abbb'])).toBe('ab')
+    expect(func(['abc', 'abdf', 'abbb'])).toBe('ab');
+    expect(func(['aa', 'aa'])).toBe('aa');
+  })
+  test('boundary test', () => {
+    expect(func([])).toBe('')
+    expect(func(['', ''])).toBe('');
   })
 })
