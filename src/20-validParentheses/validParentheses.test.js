3d174a6dde99f1c869a247cd87744a7a194879a0
徐亚光 <xuyaguang@xuyaguangdeMBP.lan>
2018-03-12 23:48:19 +0800

    add Q20

---
+++ src/20-validParentheses/validParentheses.test.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/valid-parentheses/description/
+
+const func = require('./validParentheses');
+
+describe('longest common prefix', () => {
+  test('common test', () => {
+    expect(func('([)]')).toBe(false);
+    expect(func('[(())]{}')).toBe(true);
+  })
+})
