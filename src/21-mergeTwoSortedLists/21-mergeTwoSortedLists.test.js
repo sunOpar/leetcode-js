dd932790df81a06c0dccf65f15e2cfa53a00bf8c
徐亚光 <xygjsj@139.com>
2018-03-20 21:28:57 +0800

    add test

---
+++ src/21-mergeTwoSortedLists/21-mergeTwoSortedLists.test.js
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/description/
+const func = require("./21-mergeTwoSortedLists.js");
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+function createList(arr) {
+  var Node = new ListNode(arr.shift());
+  console.log(Node)
+  arr.reduce((prev, current) => {
+    prev.next = new ListNode(current);
+    return prev.next
+  }, Node);
+  return Node;
+}
+
+describe("merge Two Sorted Lists", () => {
+  test("common test", () => {
+    const a = createList([1, 2, 4]);
+    const b = createList([1, 3, 4]);
+    const c = createList([1, 1, 2, 3, 4, 4]);
+    expect(func(a, b)).toEqual(c);
+  });
+});
